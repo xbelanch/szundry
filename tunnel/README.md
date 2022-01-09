12dd0c48f3c075dc774a4b5baad576309b3ae2b0
Xavier Belanche <xbelanch@protonmail.com>
2022-01-09 20:20:24 +0100

    Works half bottom of screen and add new references

--- tunnel/README.md
+++ tunnel/README.md
@@ -4,4 +4,7 @@ Simple tunnel effect SDL2 demo.
 
 ## References
 
+- [lodev's tunnel effect](https://lodev.org/cgtutor/tunnel.html)
+- [3D Tunnel](http://benryves.com/tutorials/tunnel/all)
+- [Tunnel effect](https://fabiensanglard.net/Tunnel/)
 - [Patater Demo Kit](https://github.com/Patater/demokit)
--- tunnel/tunnel2.c
+++ tunnel/tunnel2.c
@@ -3,8 +3,10 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <SDL2/SDL.h>
-#define WINDOW_WIDTH 320
-#define WINDOW_HEIGHT 240
+#define WINDOW_WIDTH 512
+#define WINDOW_HEIGHT 512
+#define TEXTURE_WIDTH 512
+#define TEXTURE_HEIGHT 512
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "./stb_image.h"
@@ -99,7 +101,6 @@ SDL_Surface *generateProceduralSurface(size_t width, size_t height)
     for (int j = 0; j < surface->h; ++j) {
         for (int i = 0 ; i < surface->w; ++i) {
             pixels[j * surface->w + i] = (i ^ j) << 24 | (i ^ j) << 16 | (i ^ j) << 8 | 0xff;
-
         }
     }
 
@@ -107,22 +108,66 @@ SDL_Surface *generateProceduralSurface(size_t width, size_t height)
     return surface;
 }
 
+
+// Generate non-linear transformation table, now for the bigger buffers (twice as big)
+struct LookupTable {
+    int *distance;
+    int *angle;
+};
+
+typedef struct LookupTable Table;
+
+// Calculate lookup tables
+// ----------------------
+//
+// The Y-Coordinate is related to the *distance* down the tunnel or *depth*.
+//
+// Math: depth = big_number / (x^2 + y^2)
+//
+// big number: (2<<23)
+//
+// Lodev explanation: The distance table contains for every pixel of the screen, the inverse of the distance to the center of the screen this pixel has. This gives pixels of the center of the screen a very high value (those are very far away, as you can see on the picture above), while the pixels on the sides of the screen get a low value (these pixels represent parts of the tunnel close to the camera).
+//
+// The angle buffer calculates the angle of the current pixel (the angle it has to the pixel in the center of the screen), by using the atan2 function. The atan2 function belongs to the <cmath> header, and returns the angle in radians of a given point by giving its x and y coordinate. It's divided through pi, so that the texture will be wrapped exactly one time around the tunnel.
+void createLookUpTable(Table *table, int widthScreen, int heightScreen)
+{
+    table->distance = malloc(sizeof(int) * widthScreen * heightScreen);
+    table->angle = malloc(sizeof(int) * widthScreen * heightScreen);
+
+    float big_number = (60.0 * TEXTURE_HEIGHT);
+    for (int row = 0; row < heightScreen; ++row) {
+        for (int col = 0; col < widthScreen; ++col) {
+            float inverse_distance = sqrt(
+                                          ((col - widthScreen / 2.0) * (col - widthScreen / 2.0))
+                                          +
+                                          ((row - heightScreen / 2.0) * (row - heightScreen / 2.0)));
+            table->distance[row * widthScreen + col] = ((int)(big_number / inverse_distance)) % TEXTURE_HEIGHT;
+            table->angle[row * widthScreen + col] = (int)(0.5 * TEXTURE_WIDTH *
+                                                                    atan2(row - heightScreen / 2.0, col - widthScreen / 2.0) / 3.141516);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv[0];
+    Table tunnel = {0};
+    createLookUpTable(&tunnel, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     scc(SDL_Init(SDL_INIT_VIDEO));
     scv();
     SDL_Window *window =
         scp(SDL_CreateWindow("Tunnel Effect", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE));
 
+
     SDL_Renderer *renderer =
         scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
     scs(renderer);
 
-    SDL_Surface *surface = scp(generateProceduralSurface(WINDOW_WIDTH, WINDOW_HEIGHT));
+    SDL_Surface *surface = scp(generateProceduralSurface(TEXTURE_WIDTH, TEXTURE_HEIGHT));
     SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, surface->w, surface->h);
+
     bool quit = false;
     while (!quit) {
         SDL_Event event = {0};
@@ -141,21 +186,22 @@ int main(int argc, char *argv[])
         void *pixels;
         SDL_LockTexture(texture, 0, &pixels, &(surface->pitch));
         memcpy(pixels, surface->pixels, surface->w * surface->h * 4);
-        Uint32 *upixels = (Uint32 *)pixels;
+        Uint32 *buffer = (Uint32 *)pixels;
+        Uint32 *pixel = surface->pixels;
         // Get or modify pixels
-        #define TILE_W 32
-        #define TILE_H 32
-        for (size_t i = 0; i < TILE_H; ++i) {
-            for (size_t j = 0; j < TILE_W; ++j) {
-                upixels[i * surface->w + j] = 0xff00ffff;
+        for (size_t i = 0; i < TEXTURE_HEIGHT; ++i) {
+            for (size_t j = 0; j < TEXTURE_WIDTH; ++j) {
+                int c_x = (tunnel.distance[i * TEXTURE_WIDTH + j]) % TEXTURE_WIDTH;
+                int c_y = (tunnel.angle[i * TEXTURE_WIDTH + j]) % TEXTURE_HEIGHT;
+                // Get the pixel color from surface
+                Uint32 color = pixel[c_y * surface->w + c_x];
+                // and copy to buffer
+                buffer[i * TEXTURE_WIDTH + j] = color;
             }
         }
         SDL_UnlockTexture(texture);
-
         // Copy texture to renderer
         scc(SDL_RenderCopy(renderer, texture, 0, 0));
-
-
         // Display VRAM data to Window
         SDL_RenderPresent(renderer);
         // Delay 100 milliseconds
@@ -248,31 +294,6 @@ int main(int argc, char *argv[])
 
 // 	}
 
-// }
-
-
-// //generate non-linear transformation table, now for the bigger buffers (twice as big)
-
-// void generateDistanceTable(Tunnel* tunnel, Screen *screen)
-// {
-
-// 	for (int y = 0; y < tunnel->bitmap->height * 2;  y++)
-// 	{
-
-// 		for (int x = 0; x < tunnel->bitmap->width * 2; x++)
-// 		{
-// 			int angle, distance;
-// 			float ratio = 64.0;
-
-
-// 			distance = (int)(ratio * texHeight / sqrt((float)((x - screen->render_w) * (x - screen->render_w) + (y - screen->render_h) * (y - screen->render_h)))) % texHeight;
-// 			angle = (unsigned int)(0.5 * texWidth * atan2((float)(y - screen->render_h), (float)(x - screen->render_w)) / 3.1416);
-
-
-// 			tunnel->distanceTable[y][x] = distance;
-// 			tunnel->angleTable[y][x] = angle;
-// 		}
-// 	}
 // }
 
 // void updateTunnel(Tunnel *tunnel, Screen *screen)
