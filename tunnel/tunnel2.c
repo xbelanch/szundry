c0e36fea86b539be7f6712aa2dc7950936b58ebf
Xavier Belanche <xbelanch@protonmail.com>
2022-01-15 21:27:31 +0100

    Look ma, without sdl surfaces!

--- tunnel/tunnel2.c
+++ tunnel/tunnel2.c
@@ -5,8 +5,7 @@
 #include <SDL2/SDL.h>
 #define WINDOW_WIDTH 512
 #define WINDOW_HEIGHT 512
-#define TEXTURE_WIDTH 512
-#define TEXTURE_HEIGHT 512
+#define TEXTURE_SIZE 256
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "./stb_image.h"
@@ -44,15 +43,16 @@ void createLookUpTable(Table *table, int widthScreen, int heightScreen)
     table->distance = malloc(sizeof(int) * widthScreen * heightScreen);
     table->angle = malloc(sizeof(int) * widthScreen * heightScreen);
 
-    float big_number = (30.0 * TEXTURE_HEIGHT);
+    float big_number = (30.0 * TEXTURE_SIZE);
     for (int row = 0; row < heightScreen; ++row) {
         for (int col = 0; col < widthScreen; ++col) {
             float inverse_distance = sqrt(
                                           ((col - widthScreen / 2.0) * (col - widthScreen / 2.0))
                                           +
                                           ((row - heightScreen / 2.0) * (row - heightScreen / 2.0)));
-            table->distance[row * widthScreen + col] = (unsigned int)(big_number / inverse_distance) % TEXTURE_HEIGHT;
-            table->angle[row * widthScreen + col] = (unsigned int)(0.5 * TEXTURE_WIDTH *
+
+            table->distance[row * widthScreen + col] = (unsigned int)(big_number / inverse_distance) % TEXTURE_SIZE;
+            table->angle[row * widthScreen + col] = (unsigned int)(0.5 * TEXTURE_SIZE *
                                                                     atan2(row - heightScreen / 2.0, col - widthScreen / 2.0) / 3.141516);
         }
     }
@@ -62,21 +62,30 @@ int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv[0];
+
+    Uint32 texture[WINDOW_WIDTH * WINDOW_HEIGHT];
+    Uint32 buffer[WINDOW_WIDTH * WINDOW_HEIGHT];
+
     Table tunnel = {0};
     createLookUpTable(&tunnel, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Generate texture
+    for (int y = 0; y < WINDOW_HEIGHT; ++y) {
+        for (int x = 0; x < WINDOW_WIDTH; ++x) {
+            texture[y * WINDOW_WIDTH + x] = (y ^ x) << 24 | (y ^ x) << 16 | (y ^ x) << 8 | 0xff;
+        }
+    }
 
     scc(SDL_Init(SDL_INIT_VIDEO));
     scv();
     SDL_Window *window =
         scp(SDL_CreateWindow("Tunnel Effect", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE));
 
-
     SDL_Renderer *renderer =
         scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
     scs(renderer);
 
-    SDL_Surface *surface = scp(generateProceduralSurface(TEXTURE_WIDTH, TEXTURE_HEIGHT));
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, surface->w, surface->h);
+    SDL_Texture *gpu_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     bool quit = false;
     while (!quit) {
@@ -89,41 +98,34 @@ int main(int argc, char *argv[])
             }
         }
 
-        scc(SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xff));
+        scc(SDL_SetRenderDrawColor(renderer, 0xff, 0x0, 0xff, 0xff));
         scc(SDL_RenderClear(renderer));
 
         // Manipulate texture
         void *pixels;
-        SDL_LockTexture(texture, 0, &pixels, &(surface->pitch));
-        memcpy(pixels, surface->pixels, surface->w * surface->h * 4);
-        Uint32 *buffer = (Uint32 *)pixels;
-        Uint32 *pixel = surface->pixels;
-        // Get or modify pixels
-        for (size_t i = 0; i < TEXTURE_HEIGHT; ++i) {
-            for (size_t j = 0; j < TEXTURE_WIDTH; ++j) {
-                int c_x = (tunnel.distance[i * TEXTURE_WIDTH + j]) % TEXTURE_WIDTH;
-                int c_y = (tunnel.angle[i * TEXTURE_WIDTH + j]) % TEXTURE_HEIGHT;
-                // Get the pixel color from surface
-                Uint32 color = pixel[c_y * surface->w + c_x];
-                // and copy to buffer
-                buffer[i * TEXTURE_WIDTH + j] = color;
+        int pitch = 4;
+
+        // Initialize the buffer
+        for (int y = 0; y < WINDOW_HEIGHT; ++y) {
+            for (int x = 0; x < WINDOW_WIDTH; ++x) {
+                unsigned int c_x = tunnel.distance[y * WINDOW_WIDTH + x] % 128;
+                unsigned int c_y = tunnel.angle[y * WINDOW_WIDTH + x] % 128;
+                Uint32 color = texture[c_y * WINDOW_WIDTH + c_x];
+                buffer[y * WINDOW_WIDTH + x] = color;
             }
         }
-        SDL_UnlockTexture(texture);
-        // Copy texture to renderer
-        scc(SDL_RenderCopy(renderer, texture, 0, 0));
+
+        SDL_LockTexture(gpu_texture, NULL, &pixels, &pitch);
+        memcpy(pixels, &buffer, WINDOW_WIDTH * WINDOW_WIDTH * 4);
+        SDL_UnlockTexture(gpu_texture);
+
+        // Copy gpu texture data ro VRAM
+        scc(SDL_RenderCopy(renderer, gpu_texture, 0, 0));
         // Display VRAM data to Window
         SDL_RenderPresent(renderer);
         // Delay 100 milliseconds
         SDL_Delay(100);
     }
-
-    // Destroy all the shitty stuff
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return (0);
 }
